bf43a7a558f856f52214b89a30b203bf57562498
alanteadams <alanteadams48@yahoo.com>
2021-05-30 14:36:06 -0500

    ajax and register ajax call

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,17 @@ $(document).on("submit", "form.js-register", function (event) {
   const _form = $(this);
   const _error = $(".js-error", _form);
 
-  const data = {
+  const dataObj = {
     email: $("input[type='email']", _form).val(),
     password: $("input[type='password']", _form).val(),
   };
 
-  if (data.email.length < 6) {
+  if (dataObj.email.length < 6) {
     _error.text("Please enter a valid email address").show();
     return false;
   }
 
-  if (data.password.length < 11) {
+  if (dataObj.password.length < 11) {
     _error
       .text("Please enter a passphrase that is at least 11 characters long.")
       .show();
@@ -23,6 +23,29 @@ $(document).on("submit", "form.js-register", function (event) {
 
   // Assuming the code gets this far, we can start the ajax process
   _error.hide();
+  //jquery ajax
+  $.ajax({
+    type: "POST",
+    url: "/php-login-course/ajax/register.php",
+    data: dataObj,
+    dataType: "json",
+    async: true,
+  })
+    .done(function ajaxDone(data) {
+      // Whatever data is
+      console.log(data);
+      if (data.redirect !== undefined) {
+        // window.location = data.redirect;
+      }
+
+      alert(data.name);
+    })
+    .fail(function ajaxFailed(e) {
+      console.error(e);
+    })
+    .always(function ajaxAlwaysDoThis(data) {
+      console.log("Always");
+    });
 
   return false;
 });
