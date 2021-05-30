7bde4541e40a826830958fb6fb11883066352ef8
alanteadams <alanteadams48@yahoo.com>
2021-05-30 10:36:03 -0500

    Add configL add login/reg pages; added footer

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+  // If there is no constant defined call __CONFIG__. DO NOT load this file
+  if(!defined('__CONFIG__')) {
+    exit('You do not have a config file');
+    // We can redirect someone in here instead of exiting or give them a 404 page
+  }
+
+  // Our config is below
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+// Allow the config
+define('__CONFIG__', true);
+// Require the config
+require_once "inc/config.php";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -17,45 +25,15 @@
 
   <body>
     <div class="uk-section uk-container">
-      <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-        <form class="uk-form-stacked js-login">
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text">Email</label>
-            <div class="uk-form-controls">
-              <input
-                class="uk-input"
-                id="form-stacked-text"
-                type="email"
-                required="required"
-                placeholder="email@email.com"
-              />
-            </div>
-          </div>
-          <div class="uk-margin">
-            <label class="uk-form-label" for="form-stacked-text"
-              >Password</label
-            >
-            <div class="uk-form-controls">
-              <input
-                class="uk-input"
-                id="form-stacked-text"
-                type="password"
-                required="required"
-                placeholder="Your Password"
-              />
-            </div>
-          </div>
-          <div class="uk-margin">
-            <button class="uk-button uk-button-default" type="submit">
-              Login
-            </button>
-          </div>
-        </form>
-      </div>
+      <?php echo "Hello World, Today is: ";
+            echo date("Y m d");
+     ?>
+     <p>
+       <a href="/php-login-course/login.php">Login</a>
+       <a href="/php-login-course/register.php">Register</a>
+     </p>
     </div>
 
-    <!-- UIkit JS -->
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit-icons.min.js"></script>
+    <?php require_once "inc/footer.php"; ?>
   </body>
 </html>
